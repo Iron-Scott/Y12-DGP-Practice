ce3023880dad21a5e1f17ae50e1dff0bfb8e68e4
Iron-Scott <78768258+Iron-Scott@users.noreply.github.com>
2021-06-01 12:42:22 +1200

    Added Config, and Changed the website layouts

--- config.php
+++ config.php
@@ -2,7 +2,7 @@
 
 ini_set("display_errors", 'On'); // change to "Off" when live
 
-define ("DB_NAME","00_12 games")
+define ("DB_NAME","cube_results")
 define ("DB_USERNAME","root")
 define ("DB_PASSWORD","");
 define ("DB_HOST","localhost");
---
+++ index.php
@@ -0,0 +1,63 @@
+<!DOCTYPE html>
+
+<html lang="en">
+
+	<head>
+		<meta charset="utf-8">
+		
+		<!-- For assessment you need to change these -->
+		<meta name="description" content="games, cube">
+		<meta name="keywords" content="Cube Timer, Rubik's Timer, Timer">
+		<meta name="keywords" content="timing">
+		
+		<title>Cubist</title>
+		
+		<!-- for multiple fonts change | to %7c * no spaces* -->
+		<link href="http://fonts.googleapis.com/css?family=Lato%7cUbuntu"
+		rel="stylesheet">
+		
+		<link rel"stylesheet" href="css/font-awesome.min.css">
+		<link rel="stylesheet" href="css/data_style.css"> <!--custom style sheet -->
+	</head>
+	
+	<body>
+		<p class="message">Eek! Your browser does not support grid. 
+		Please upgrade your system.</p>
+		
+		<div class="wrapper">
+			
+			<!-- logo / small image goes here -->
+			<div class="box logo">
+				<a href="index.php"><img src="images/logo.png"
+				width ="261"
+				height="150"
+				alt="Dice" /></a>
+			</div>
+			
+			<div class="box banner">
+				<h1>Cubist</h1>
+			</div> <!-- / banner -->
+			
+			<div class="box main">
+				<h2>Welcome</h2>
+				
+				<p>
+					Put link to original dataset here
+				</p>
+			</div> <!-- / main -->
+			
+			<div class="box side">
+				<h2>Search Area</h2>
+				
+				<p>
+					This is where the search area goes
+				</p>
+			</div> <!-- / side -->
+			
+			<div class="box footer">
+				<p>Mr Scott Henderson 2021</p>
+			</div> <!-- / footer -->
+			
+		</div> <!-- / wrapper -->
+	</body>
+</html>
